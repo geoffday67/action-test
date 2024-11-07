9c81c3f0999a716ee0e65d2c9aba98c70109554f
Geoff Day <geoff.day@justeattakeaway.com>
2024-11-07 16:20:28 +0000

    Test action

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
 
+    core.debug(`Geoff is great!`)
+
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
 
