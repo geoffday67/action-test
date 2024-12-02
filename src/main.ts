4b0da7cfc269571fce611e05cc5a161c168d2ea0
Geoff Day <geoff.day@justeattakeaway.com>
2024-12-02 17:29:26 +0000

    Require JSON

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
-import * as data from './sample.json';
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
+
+type PhraseError = {
+  key: string,
+  message: string,
+}
+
+type PhraseTag = {
+  tag: string,
+  errors: PhraseError[],
+}
+
 export async function run(): Promise<void> {
-  core.summary.addHeading(data.name, 2);
+  const data: PhraseTag[] = require("./phrase-report.json");
+
+  data.forEach(tag => {
+    core.summary.addHeading(tag.tag, 3);
+  
+    tag.errors.forEach(error => {
+      core.summary.addHeading(error.message, 4);
+    })
+  });
+
   core.summary.write();
 }
