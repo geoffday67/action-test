9352d5ba4d06db20c0f975a78e0622f4ffc28bab
Geoff Day <geoff.day@justeattakeaway.com>
2024-12-02 16:06:16 +0000

    Read JSON

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,12 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
+import * as data from './sample.json';
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ... Geoff is lovely!`)
-
-    core.summary.addList(["Geoff", "is", "great!"]);
-    core.summary.write();
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  core.summary.addHeading(data.name, 2);
+  core.summary.write();
 }
